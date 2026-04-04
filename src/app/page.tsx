9e404b4e6f45c26a70388a300745d5b18251305a
Juhyung03 <159880204+Juhyung03@users.noreply.github.com>
2026-04-04 22:42:00 +0900

    [CHORE]: Initialize project structure and style setup (#4)
    * chore: src 폴더구조 세팅 (#3)
    
    * chore: global font & global color 지정 (#3)
    
    * chore: 폴더 구조에 default export 추가 (#3)
    
    ---------
    
    Co-authored-by: juhyung <njh1019@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,3 @@
 export default function Home() {
-  return (
-    <div className='flex min-h-screen items-center justify-center bg-zinc-50 dark:bg-zinc-950'>
-      <h1 className='text-5xl font-bold tracking-tighter text-blue-600 dark:text-blue-400'>
-        Hello World
-      </h1>
-    </div>
-  );
+  return <></>;
 }
