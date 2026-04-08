fcf5b8d98dcce74aa34f4ef053e3a2d92855bb3c
Juhyung03 <159880204+Juhyung03@users.noreply.github.com>
2026-04-08 13:04:28 +0900

    [FEAT]: 로딩 페이지 퍼블리싱 & SVG 타입 추가 (#7)
    * chore: svg 타입 지원 추가 (#5)
    
    * chore: Header.tsx / NavBar.tsx 디렉토리 생성 및 layout 추가 (#5)
    
    * chore: 프로젝트 assets에 loading 페이지 svg 이미지 추가 (#5)
    
    * feat: loading 페이지 퍼블리싱 (#5)
    
    ---------
    
    Co-authored-by: juhyung <njh1019@gmail.com>

--- src/app/main/result/loading/page.tsx
+++ src/app/main/result/loading/page.tsx
@@ -1,3 +1,27 @@
+import WaveForm from '@/assets/loading/waveform.svg';
+import LogoCircleContainer from '@/assets/loading/logocirclecontainer.svg';
+
 export default function Loadingpage() {
-  return <div>page</div>;
+  return (
+    <div className='flex h-screen flex-col items-center p-8'>
+      <div className='flex flex-col items-center gap-9'>
+        <p className='text-primary/60 text-xs font-bold tracking-widest'>
+          ANALYSIS IN PROGRESS
+        </p>
+        <LogoCircleContainer alt='로고 원형 컨테이너' />
+        <WaveForm alt='음성 파형' />
+        <p className='text-primary text-2xl font-bold'>심층 분석 중...</p>
+        <p className='text-primary2 text-center text-sm font-bold'>
+          잠시만 기다려주세요.
+          <br />
+          당신의 발음, 속도, 그리고 표현력을 인공지능이 세밀하게
+          <br />
+          파헤치고 있습니다.
+        </p>
+      </div>
+      <p className='text-brown mt-auto text-[10px] font-extrabold'>
+        TALKTALK COACH MINDSET
+      </p>
+    </div>
+  );
 }
