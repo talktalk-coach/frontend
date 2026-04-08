50bbedc85c196b5174155aa3ebba973502642ad3
juhyung <njh1019@gmail.com>
2026-04-08 12:30:00 +0900

    chore: Header.tsx / NavBar.tsx 디렉토리 생성 및 layout 추가 (#5)

--- src/app/main/layout.tsx
+++ src/app/main/layout.tsx
@@ -1,3 +1,12 @@
-export default function MainLayout() {
-  return <div>layout</div>;
+import {Header} from '@/components/layout/Header';
+import {NavBar} from '@/components/layout/NavBar';
+
+export default function MainLayout({children}: {children: React.ReactNode}) {
+  return (
+    <div className='bg-background min-h-screen'>
+      <Header />
+      {children}
+      <NavBar />
+    </div>
+  );
 }
