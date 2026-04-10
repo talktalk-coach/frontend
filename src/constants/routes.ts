9ad8b9dab4354e0992b9658017e9694721a84a6b
Juhyung03 <159880204+Juhyung03@users.noreply.github.com>
2026-04-10 20:52:09 +0900

    [FEAT]: 분석 결과 페이지 퍼블리싱 (#9)
    * feat: result 페이지 관련 asset 이미지 추가 (#8)
    
    * feat: header 관련 asset 이미지 추가 (#8)
    
    * style: gray 색상 커스텀 추가 (#8)
    
    * feat: routes 경로 설정 (#8)
    
    * chore: result 페이지 관련 mock 데이터 설정 (#8)
    
    * design: loading 페이지 디자인 수정 (#8)
    
    * feat: 헤더 컴포넌트 퍼블리싱 (#8)
    
    * feat: 결과 페이지 관련 컴포넌트 퍼블리싱 (#8)
    
    * feat: 결과 페이지 퍼블리싱 (#8)
    
    ---------
    
    Co-authored-by: juhyung <njh1019@gmail.com>

---
+++ src/constants/routes.ts
@@ -0,0 +1,8 @@
+export const ROUTES = {
+  MAIN: '/main',
+  HOMEPAGE: '/homepage',
+  RECORD: '/main/record',
+  RESULT_LOADING: '/main/result/loading',
+  RESULT_SHARE: '/main/result/share',
+  MYPAGE: '/mypage',
+};
