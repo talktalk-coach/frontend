6ea5902e43cba0b422e9274834cee8fefd3e64c2
seoyeon.cho <jsy20030521@gmail.com>
2026-04-16 10:29:16 +0900

    style: result 하단 공백 추가

--- src/app/main/result/page.tsx
+++ src/app/main/result/page.tsx
@@ -13,7 +13,7 @@ import {ROUTES} from '@/constants/routes';
 
 export default function Resultpage() {
   return (
-    <div className='flex flex-col gap-10 px-6 pt-10'>
+    <div className='flex flex-col gap-10 px-6 pt-10 pb-10'>
       <section className='gap-10 rounded-[40px] bg-[#606C38] p-6'>
         <h2 className='font-pretendard text-3xl font-bold text-[#DFEDAC]'>
           Excellent Progress!
