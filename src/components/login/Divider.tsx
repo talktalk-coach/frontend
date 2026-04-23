31bc3b087425f33aa6fea36b4d1f6a0a3ceabd8a
juhyung <njh1019@gmail.com>
2026-04-23 23:45:00 +0900

    feat: 로그인 페이지 및 컴포넌트 퍼블리싱 (#18)

---
+++ src/components/login/Divider.tsx
@@ -0,0 +1,11 @@
+export const Divider = () => {
+  return (
+    <div className='flex w-full items-center'>
+      <div className='flex-grow border-1 border-gray-200 shadow'></div>
+      <span className='text-shadow text-primary/70 mx-4 text-[10px] font-extrabold tracking-widest'>
+        OR CONTINUE WITH
+      </span>
+      <div className='flex-grow border-1 border-gray-200 shadow'></div>
+    </div>
+  );
+};
