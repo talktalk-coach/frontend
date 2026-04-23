63b5425a794e93c8ad2e40ae1954d79532e79ed0
juhyung <njh1019@gmail.com>
2026-04-23 23:46:42 +0900

    feat: 랜딩 페이지 및 컴포넌트 퍼블리싱 (#18)

---
+++ src/components/landing/Footer.tsx
@@ -0,0 +1,10 @@
+export const Footer = () => {
+  return (
+    <footer className='bg-surface fixed bottom-0 w-full rounded-t-[48px] p-5 text-center'>
+      <h2 className='text-primary mb-2 text-lg font-bold'>TalkTalk Coach</h2>
+      <p className='text-primary2 text-xs'>
+        © 2026 TalkTalk Coach. All rights reserved.
+      </p>
+    </footer>
+  );
+};
