8699062a27f25446b1198b73cde7f9d7325547f3
juhyung <njh1019@gmail.com>
2026-04-25 17:03:22 +0900

    refactor: 페이지 추가 및 폴더 구조 재구성 (#23)

--- src/app/mypage/difficulty/page.tsx
+++ src/app/mypage/difficulty/page.tsx
@@ -1,7 +1,7 @@
-export default function Signuppage() {
+export default function page() {
   return (
     <div className='flex min-h-screen items-center justify-center'>
-      회원가입 페이지
+      난이도(학년)변경 페이지
     </div>
   );
 }
--- src/app/mypage/layout.tsx
+++ src/app/mypage/layout.tsx
@@ -1,12 +1,3 @@
-import {Header} from '@/components/layout/Header';
-import {NavBar} from '@/components/layout/NavBar';
-
 export default function MypageLayout({children}: {children: React.ReactNode}) {
-  return (
-    <div className='bg-background min-h-screen'>
-      <Header />
-      {children}
-      <NavBar />
-    </div>
-  );
+  return <div className='bg-background min-h-screen'>{children}</div>;
 }
