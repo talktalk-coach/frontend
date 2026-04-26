ef285bfe4e09eccfd5de321019e3c01c53e0e324
seoyeon.cho <jsy20030521@gmail.com>
2026-04-27 00:09:57 +0900

    refactor: 사용하지 않는 CircleIcon variant 제거

--- src/components/common/display/CircleIcon.tsx
+++ src/components/common/display/CircleIcon.tsx
@@ -1,15 +1,13 @@
 import Icon from '@/assets/icons/Speech-off.svg';
 
-type CircleIconVariant = 'large' | 'medium' | 'landing' | 'auth';
+type CircleIconVariant = 'landing' | 'auth';
 
 type CircleIconProps = {
-  variant?: CircleIconVariant;
+  variant: CircleIconVariant;
 };
 
-export const CircleIcon = ({variant = 'large'}: CircleIconProps) => {
+export const CircleIcon = ({variant}: CircleIconProps) => {
   const styles = {
-    large: 'w-1/2 border-6 shadow-2xl',
-    medium: 'w-2/5 border-4 shadow-2xl',
     landing:
       'w-[170px] border-8 shadow-[0_20px_48px_-12px_rgba(26,28,23,0.04)] bg-surface',
     auth: 'w-[112px] border-4 shadow-[0_1px_2px_rgba(0,0,0,0.05)] bg-surface',
