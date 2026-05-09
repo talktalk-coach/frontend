209ef33bc3ad8515537bb05a4d82600ea090c3bb
juhyung <njh1019@gmail.com>
2026-05-09 14:10:22 +0900

    feat: footer 버튼 클릭시 페이지 이동 (#28)

--- src/components/auth/signup/SignupFooter.tsx
+++ src/components/auth/signup/SignupFooter.tsx
@@ -1,8 +1,17 @@
+import {useRouter} from 'next/navigation';
+import {ROUTES} from '@/constants/routes';
+
 export const SignupFooter = () => {
+  const router = useRouter();
+
   return (
     <div className='mt-7 flex gap-4 text-xs font-bold'>
       <span className='text-primary2'>이미 계정이 있으신가요?</span>
-      <button className='text-primary'>로그인하기</button>
+      <button
+        className='text-primary'
+        onClick={() => router.push(ROUTES.LOGIN)}>
+        로그인하기
+      </button>
     </div>
   );
 };
